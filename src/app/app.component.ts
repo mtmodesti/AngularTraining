6765f74c463327da77c207a28e31c08a60a50309
mtmodesti <marcelomodesti1988@gmail.com>
2022-07-13 19:13:21 -0300

    feat: conditional rendering component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Marcelo'
+  userData = {name: 'Marcelo', role:"Admin"}
   title = 'tuto-ang';
 }
